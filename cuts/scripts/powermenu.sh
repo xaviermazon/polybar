981c59d5c60749645dac06cd012ae6885934030a
xavi <xavi@asus>
2022-08-03 12:31:01 +0200

    Subida de ficheros para polybar

---
+++ cuts/scripts/powermenu.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="~/.config/polybar/cuts/scripts/rofi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Options
+shutdown=" Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend=" Sleep"
+logout=" Logout"
+
+# Variable passed to rofi
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+		systemctl poweroff
+        ;;
+    $reboot)
+		systemctl reboot
+        ;;
+    $lock)
+		if [[ -f /usr/bin/i3lock ]]; then
+			i3lock
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+		systemctl suspend
+        ;;
+    $logout)
+		if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+			openbox --exit
+		elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+			bspc quit
+		elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+			i3-msg exit
+		fi
+        ;;
+esac
