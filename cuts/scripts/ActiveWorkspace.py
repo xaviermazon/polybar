d1c1669deb08bffcc519ec50663006ae5625932c
Xavier Mazon <xavi.mazon.bcn@gmail.com>
2022-08-08 12:19:04 +0200

    Commit maintainer repository

---
+++ cuts/scripts/ActiveWorkspace.py
@@ -0,0 +1,18 @@
+from i3ipc import Connection, Event
+
+i3 = Connection()
+
+focused = i3.get_tree().find_focused().workspace().num
+
+out = ""
+
+for i in range(1, 11):
+    if i != 1:
+        out += " "
+
+    if i == focused:
+        out += "%{F#a84}[" + str(i) + "]%{F-}"
+    else:
+        out += str(i) + "%{F-}"
+
+print(out)
