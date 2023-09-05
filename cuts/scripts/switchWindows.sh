a1c8e6a76c006fbb17c1cd614dcc34e027d8d97c
Xavier Mazon <xavi.mazon.bcn@gmail.com>
2023-09-05 15:46:38 +0200

    Fix switchWindow

--- cuts/scripts/switchWindows.sh
+++ cuts/scripts/switchWindows.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
-
 dir="~/.config/polybar/cuts/scripts/rofi"
 
 rofi_command="rofi -theme $dir/switchWindows.rasi"
 
 chosen="$(echo -e "$options" | $rofi_command -show window -selected-row 0)"
-=======
-rofi -show window &
->>>>>>> origin
